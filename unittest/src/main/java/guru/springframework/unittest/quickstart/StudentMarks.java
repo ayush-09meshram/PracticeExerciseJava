b55fb574ca11e207f13e0e1998c3597a5825cca6
ayush.meshram <ayush.meshram@cgi.com>
2018-09-17 18:19:09 +0530

    Third exercise

---
+++ unittest/src/main/java/guru/springframework/unittest/quickstart/StudentMarks.java
@@ -0,0 +1,18 @@
+package guru.springframework.unittest.quickstart;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StudentMarks{
+    public String StudMarks(int numOfStudents){
+        int[] studGrades = new int[numOfStudents];
+        String answer = "";
+        for(int i=0; i<numOfStudents; i++){
+            if(studGrades[i]>0 && studGrades[i]<100){
+                
+            }
+        }
+
+    }
+}
