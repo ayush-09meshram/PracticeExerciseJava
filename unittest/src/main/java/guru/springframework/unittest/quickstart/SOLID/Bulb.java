9099de3ba293e069c191c5f27578aa03093f4400
ayush.meshram <ayush.meshram@cgi.com>
2018-09-20 09:33:13 +0530

    UPDATED

---
+++ unittest/src/main/java/guru/springframework/unittest/quickstart/SOLID/Bulb.java
@@ -0,0 +1,16 @@
+package guru.springframework.unittest.quickstart;
+
+import org.junit.Test;
+
+import java.util.*;
+import java.io.*;
+import java.util.Scanner;
+
+import static org.junit.Assert.*;
+
+public class Bulb{
+    public int i = 0;
+        public boolean Operator(String){
+
+        }
+}
---
+++ unittest/src/main/java/guru/springframework/unittest/quickstart/SOLID/Operation.java
@@ -0,0 +1,15 @@
+package guru.springframework.unittest.quickstart;
+
+import org.junit.Test;
+
+import java.util.*;
+import java.io.*;
+import java.util.Scanner;
+
+import static org.junit.Assert.*;
+
+public class Operation extends Bulb {
+        public boolean Switch(String command){
+
+        }
+}
